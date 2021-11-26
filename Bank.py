321a7abac1126e370619faa2a09e8c4f5e845950
나혜주 <hyejuna@nahyejuui-MacBook-Pro-2.local>
2021-11-26 16:06:10 +0900

    create class Bank

---
+++ Bank.py
@@ -0,0 +1,27 @@
+import Card
+import Account
+
+class Bank:
+    def __init__(self):
+        self.card = []
+        self.account = []
+
+    def addCard(self, card):
+        self.card.append(card)
+
+    def addAccount(self, account):
+        self.account.append(account)
+
+    # Find Card with Card Number / Return False if doesn't exist
+    def getCard(self, cardNumber):
+        for card in self.card:
+            if cardNumber == card.getCardNumber():
+                return card
+        return False
+
+    # Find Account with Account Number / Return False if doesn't exist
+    def getAccount(self, accountNumber):
+        for account in self.account:
+            if accountNumber == account.getAccountNumber():
+                return account
+        return False
