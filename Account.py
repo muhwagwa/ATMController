9316ddef9b40794a7b4dab0fb17aaacc2e883bf9
나혜주 <hyejuna@nahyejuui-MacBook-Pro-2.local>
2021-11-26 15:54:49 +0900

    create class Account

---
+++ Account.py
@@ -0,0 +1,37 @@
+class Account:
+    def __init__(self, username, accountNumber, password, cardNumber, balance, limit):
+        self.username = username
+        self.accountNumber = accountNumber
+        self.password = password
+        self.cardNumber = cardNumber
+        self.balance = balance
+        self.limit = limit
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount < self.limit:
+            self.balance -= amount
+            return True
+        # exceeded account limit
+        else:
+            return False
+
+    def getUserName(self):
+        return self.username
+
+    def getAccountNumber(self):
+        return self.accountNumber
+
+    def getPassword(self):
+        return self.password
+    
+    def getCardNumber(self):
+        return self.cardNumber
+
+    def getBalance(self):
+        return self.balance
+
+    def getLimit(self):
+        return self.limit
